05fae0563cfee680e9774c0d6f820f8d3af3dfec
Santinal Browns <asantikuseka@outlook.com>
2022-06-25 12:00:20 +0200

    Added tenants controller

---
+++ src/controllers/manager/tenants.ts
@@ -0,0 +1,59 @@
+import { NextFunction, Request, Response } from "express";
+import { ROLE } from "../../enum";
+import { Errors } from "../../helpers";
+import User from "../../models/User";
+
+const tenants = {
+    get: async (request: Request, response: Response, next: NextFunction) => {
+
+        try {
+
+            if (request.params.id) {
+
+
+                const tenant = await User.findOne({ _id: request.params.id, role: ROLE.TENANT });
+
+                if (!tenant) throw Errors.notFound('Tenant not found.');
+
+                response.status(200).json({
+                    id: tenant._id,
+                    firstname: tenant.firstname,
+                    lastname: tenant.lastname,
+                    email: tenant.email,
+                    created_at: tenant.createdAt,
+                    updated_at: tenant.updatedAt
+                });
+
+            }
+
+            const tenants = await User.find({ role: ROLE.TENANT });
+
+
+            if (!tenants) throw Errors.notFound('Tenants not found.');
+
+
+            const body = tenants.map((tenant) => {
+
+                return {
+                    id: tenant._id,
+                    firstname: tenant.firstname,
+                    lastname: tenant.lastname,
+                    email: tenant.email,
+                    created_at: tenant.createdAt,
+                    updated_at: tenant.updatedAt
+                }
+            })
+
+
+            response.status(200).json(body);
+
+
+        } catch (error) {
+            next(error);
+        }
+
+    }
+}
+
+export default tenants;
+
