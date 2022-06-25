9629e2ab14d38db918b0e7f205b637276d4101de
Santinal Browns <asantikuseka@outlook.com>
2022-06-25 13:34:40 +0200

    Restructured service model

--- src/models/Service.ts
+++ src/models/Service.ts
@@ -2,22 +2,13 @@ import mongoose from "mongoose";
 import { Service } from "../interfaces";
 
 const schema = new mongoose.Schema({
-    price: {
-        type: Number,
-        required: true
+    name: {
+        type: String,
+        required: true,
     },
     category: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category',
-        required: true
-    },
-    hours: {
-        type: Number,
-        required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
         required: true
     }
 });
